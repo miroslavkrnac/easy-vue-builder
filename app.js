11d2cc8b670909846e52217aff2cb00b1927d928
miroslav.krnac <miroslav.krnac@easysoftware.com>
2019-12-22 18:31:17 +0100

    get version number

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ const fs = require("fs");
 const readline = require("readline");
 const prependFile = require("prepend-file");
 const npmRun = require("npm-run");
-const git = require('simple-git/promise')
-const bundleTextToAppend = "/*! no_asset_compression */\n"; 
+const git = require("simple-git/promise");
+const bundleTextToAppend = "/*! no_asset_compression */\n";
+const exec = require("child_process");
 let bundlePath = "app/assets/javascripts/easy_vue/bundle.js";
+const versionPath = "lib/easy_vue/version.rb";
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const getSetQuestion = (question) => new Promise(res => rl.question(question, res));
+const getSetQuestion = question =>
+  new Promise(res => rl.question(question, res));
 
-const getSetDefaultQuestion = (question, defaultValue) => new Promise(res => {
-  rl.question(question, (newValue) => res(newValue.length ? newValue : defaultValue));
-});
+const getSetDefaultQuestion = (question, defaultValue) =>
+  new Promise(res => {
+    rl.question(question, newValue =>
+      res(newValue.length ? newValue : defaultValue)
+    );
+  });
 
 const finishAndExit = (branchName, commitName) => {
   console.log(
@@ -24,24 +30,50 @@ const finishAndExit = (branchName, commitName) => {
      Pushed to branch: ${branchName} \n
      Created commit: ${commitName} \n
      and pushed to branch
-    `);
+    `
+  );
   process.exit(0);
 };
 
+const getVersions = () => {
+  const isVersion = fs.existsSync(versionPath);
+  if (!isVersion) return null;
+  const version = fs.readFileSync(versionPath, "utf8");
+  const versionParsedArr = version.match(/\d+/g, version);
+  const [major, minor, patch] = versionParsedArr;
+  const currVersion = `${major}.${minor}.${patch}`;
+  const increasedVersion = `${major}.${minor}.${parseInt(patch) + 1}`;
+  return { curr: currVersion, increased: increasedVersion };
+};
 
 const init = async () => {
   try {
+    const version = getVersions();
+    console.log(version);
+    process.exit(0);
     // ask if user pushed all files
-    const pushedAndCommited = await getSetDefaultQuestion(`Have you committed and pushed all files? (yes): `, "yes");
+    const pushedAndCommited = await getSetDefaultQuestion(
+      `Have you committed and pushed all files? (yes): `,
+      "yes"
+    );
     if (pushedAndCommited.toLowerCase() !== "yes") {
       throw new Error("Cancelled by user");
     }
     git().reset("hard");
-    const {current: currentBranch} = await git().branch();
+    const { current: currentBranch } = await git().branch();
     // ask for branch name
-    const branchName = await getSetDefaultQuestion(`Branch name: ${currentBranch} (yes): `, currentBranch);
-    const commitMessage = await getSetDefaultQuestion("Commit name: new release (yes): ", "new release");   
-    bundlePath = await getSetDefaultQuestion(`Bundle path ${bundlePath} (yes): `, bundlePath);
+    const branchName = await getSetDefaultQuestion(
+      `Branch name: ${currentBranch} (yes): `,
+      currentBranch
+    );
+    const commitMessage = await getSetDefaultQuestion(
+      "Commit name: new release (yes): ",
+      "new release"
+    );
+    bundlePath = await getSetDefaultQuestion(
+      `Bundle path ${bundlePath} (yes): `,
+      bundlePath
+    );
     // if users branch !== current branch, checkout to new specified branch
     if (branchName !== currentBranch) {
       await git().checkoutLocalBranch(branchName);
@@ -52,7 +84,7 @@ const init = async () => {
     console.log("Successfully built bundle.js");
     const bundle = fs.existsSync(bundlePath);
     if (!bundle) {
-      throw new Error("Bundle not found!!")
+      throw new Error("Bundle not found!!");
     }
     // append text to bundle.js
     prependFile.sync(bundlePath, bundleTextToAppend);
@@ -63,7 +95,7 @@ const init = async () => {
     await git().commit(commitMessage);
     await git().push("origin", branchName);
     finishAndExit(branchName, commitMessage);
-  } catch(e) {
+  } catch (e) {
     console.log(e);
     process.exit(0);
   }
