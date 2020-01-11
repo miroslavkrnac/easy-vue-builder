e86f193a37a57bd3ce3a4a82b650c145a42bc356
miroslav.krnac <miroslav.krnac@easysoftware.com>
2020-01-11 18:25:06 +0100

    prompt using, refactor, version computing

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const readline = require("readline");
 const prependFile = require("prepend-file");
 const npmRun = require("npm-run");
 const git = require("simple-git/promise");
@@ -7,21 +6,7 @@ const bundleTextToAppend = "/*! no_asset_compression */\n";
 const exec = require("child_process");
 let bundlePath = "app/assets/javascripts/easy_vue/bundle.js";
 const versionPath = "lib/easy_vue/version.rb";
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-const getSetQuestion = question =>
-  new Promise(res => rl.question(question, res));
-
-const getSetDefaultQuestion = (question, defaultValue) =>
-  new Promise(res => {
-    rl.question(question, newValue =>
-      res(newValue.length ? newValue : defaultValue)
-    );
-  });
+const prompts = require("prompts");
 
 const finishAndExit = (branchName, commitName) => {
   console.log(
@@ -35,70 +20,138 @@ const finishAndExit = (branchName, commitName) => {
   process.exit(0);
 };
 
-const getVersions = () => {
+const getCurrentVersion = () => {
   const isVersion = fs.existsSync(versionPath);
   if (!isVersion) return null;
   const version = fs.readFileSync(versionPath, "utf8");
   const versionParsedArr = version.match(/\d+/g, version);
   const [major, minor, patch] = versionParsedArr;
-  const currVersion = `${major}.${minor}.${patch}`;
-  const increasedVersion = `${major}.${minor}.${parseInt(patch) + 1}`;
-  return { curr: currVersion, increased: increasedVersion };
+  return {
+    versionString: `${major}.${minor}.${patch}`,
+    major,
+    minor,
+    patch
+  };
+};
+
+const resetAndCommit = async ({ value: confirmed, aborted }) => {
+  if (!confirmed || aborted) {
+    console.log("\n You have to push and commit all data first");
+    process.exit(0);
+  }
+  git().reset("hard");
+};
+
+const getBranchChoices = async (options, prev) => {
+  const { currentBranch, computedVersions } = options;
+  const choices = [
+    { title: `${currentBranch}`, value: `${currentBranch}` },
+    { title: `Own name`, value: "own" }
+  ];
+  if (prev) {
+    const attrName = `increased${prev.charAt(0).toUpperCase()}${prev.slice(1)}`;
+    let increasedVersion = computedVersions[attrName];
+    choices.unshift({
+      title: `release_${increasedVersion}`,
+      value: `release_${increasedVersion}`
+    });
+  }
+  return choices;
+};
+
+const processResponses = async answers => {
+  const { bundlePath, commitMessage, branchName } = answers;
+  const { current: currentBranch } = await git().branch();
+
+  // if users branch !== current branch, checkout to new specified branch
+  if (branchName !== currentBranch) {
+    await git().checkoutLocalBranch(branchName);
+  }
+  // Run npm build
+  console.log("Started npm run build, please wait...");
+  npmRun.execSync("npm run build");
+  console.log("Successfully built bundle.js");
+  const bundle = fs.existsSync(bundlePath);
+  if (!bundle) {
+    throw new Error("Bundle not found!!");
+  }
+  // append text to bundle.js
+  prependFile.sync(bundlePath, bundleTextToAppend);
+  // add all changes to git
+  await git().add("*");
+  // ask for commit message
+  console.log("committed with message", commitMessage);
+  await git().commit(commitMessage);
+  await git().push("origin", branchName);
+  finishAndExit(branchName, commitMessage);
+};
+
+const computeVersions = currVersion => {
+  const { major, minor, patch } = currVersion;
+  const increasedMajor = `${+major + 1}.${minor}.${patch}`;
+  const increasedMinor = `${major}.${+minor + 1}.${patch}`;
+  const increasedPatch = `${major}.${minor}.${+patch + 1}`;
+  return {
+    increasedMajor,
+    increasedMinor,
+    increasedPatch
+  };
 };
 
 const init = async () => {
-  try {
-    const version = getVersions();
-    console.log(version);
-    process.exit(0);
-    // ask if user pushed all files
-    const pushedAndCommited = await getSetDefaultQuestion(
-      `Have you committed and pushed all files? (yes): `,
-      "yes"
-    );
-    if (pushedAndCommited.toLowerCase() !== "yes") {
-      throw new Error("Cancelled by user");
+  const currVersion = getCurrentVersion();
+  const { current: currentBranch } = await git().branch();
+
+  const computedVersions = computeVersions(currVersion);
+  const { increasedMajor, increasedMinor, increasedPatch } = computedVersions;
+  const answers = await prompts([
+    {
+      type: "confirm",
+      name: "pushedAndCommited",
+      message: "Have you commited and pushed all files?",
+      initial: true,
+      onState: resetAndCommit
+    },
+    {
+      type: "select",
+      name: "versionType",
+      message: `Pick a version you want to increase. Current version is: ${currVersion.versionString}`,
+      choices: [
+        { title: `Major ${increasedMajor}`, value: "major" },
+        { title: `Minor ${increasedMinor}`, value: "minor" },
+        { title: `Patch ${increasedPatch}`, value: "patch" },
+        { title: `None of them`, value: null }
+      ]
+    },
+    {
+      type: "select",
+      name: "branchName",
+      message: `Select branch name`,
+      choices: getBranchChoices.bind(null, {
+        currentBranch,
+        computedVersions
+      })
+    },
+    {
+      type: prev => (prev === "own" ? "text" : false),
+      name: "branchName",
+      message: `Type branch name`
+    },
+    {
+      type: "text",
+      name: "commitMessage",
+      message: `Commit message`,
+      initial: prev => `New ${prev}`
+    },
+    {
+      type: "text",
+      name: "bundlePath",
+      message: `Bundle path`,
+      initial: `${bundlePath}`
     }
-    git().reset("hard");
-    const { current: currentBranch } = await git().branch();
-    // ask for branch name
-    const branchName = await getSetDefaultQuestion(
-      `Branch name: ${currentBranch} (yes): `,
-      currentBranch
-    );
-    const commitMessage = await getSetDefaultQuestion(
-      "Commit name: new release (yes): ",
-      "new release"
-    );
-    bundlePath = await getSetDefaultQuestion(
-      `Bundle path ${bundlePath} (yes): `,
-      bundlePath
-    );
-    // if users branch !== current branch, checkout to new specified branch
-    if (branchName !== currentBranch) {
-      await git().checkoutLocalBranch(branchName);
-    }
-    // Run npm build
-    console.log("Started npm run build, please wait...");
-    npmRun.execSync("npm run build");
-    console.log("Successfully built bundle.js");
-    const bundle = fs.existsSync(bundlePath);
-    if (!bundle) {
-      throw new Error("Bundle not found!!");
-    }
-    // append text to bundle.js
-    prependFile.sync(bundlePath, bundleTextToAppend);
-    // add all changes to git
-    await git().add("*");
-    // ask for commit message
-    console.log("committed with message", commitMessage);
-    await git().commit(commitMessage);
-    await git().push("origin", branchName);
-    finishAndExit(branchName, commitMessage);
-  } catch (e) {
-    console.log(e);
-    process.exit(0);
-  }
+  ]);
+
+  await processResponses(answers);
 };
 
 init();
